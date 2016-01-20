f84cab35174b69c2ce405164743da1e73c9a50e0
delas <andrea.burattin@gmail.com>
2016-01-20 12:10:34 +0100

    Added sources of first working Java project

---
+++ src/ImgagesOrganizer/src/imgorg/core/JpegImage.java
@@ -0,0 +1,52 @@
+package imgorg.core;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.sanselan.ImageReadException;
+import org.apache.sanselan.Sanselan;
+import org.apache.sanselan.common.IImageMetadata;
+import org.apache.sanselan.formats.jpeg.JpegImageMetadata;
+import org.apache.sanselan.formats.tiff.TiffField;
+import org.apache.sanselan.formats.tiff.constants.TiffTagConstants;
+
+public class JpegImage extends Image {
+
+	protected JpegImageMetadata metadata;
+	
+	protected JpegImage(File file) {
+		super(file);
+		
+		if (Sanselan.hasImageFileExtension(file)) {
+			try {
+				this.file = file;
+				IImageMetadata metadata = Sanselan.getMetadata(file);
+				if (metadata instanceof JpegImageMetadata) {
+					this.metadata = (JpegImageMetadata) metadata;
+				}
+			} catch (ImageReadException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	@Override
+	public Date getShootDate() {
+		try {
+			TiffField dateTime = metadata.findEXIFValue(TiffTagConstants.TIFF_TAG_DATE_TIME);
+			return new SimpleDateFormat("yyyy:MM:dd HH:mm:ss").parse(dateTime.getStringValue());
+		
+		} catch (ImageReadException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
